6923a724b1590fabb393309d3d9ea0fac22b134d
Andrey <soul.evans.15@gmail.com>
2019-03-20 17:35:35 +0300

    Initial react app commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DraggableList from './app/draggable-list';
+
+// JS CODE
+
+// Rendering RoomList component
+ReactDOM.render(<DraggableList/>, document.querySelector('#draggable-sortable-list'));
